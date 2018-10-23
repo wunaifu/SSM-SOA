2e3cd9c83dc635c4235fda9e32980d2e453a89bc
wunaifu <1278022159@qq.com>
2018-10-23 09:11:30 +0800

    完整版

---
+++ taotao13/taotao-manager-web/src/main/java/com/taotao/sso/controller/ContentController.java
@@ -0,0 +1,53 @@
+package com.taotao.sso.controller;
+
+import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.content.service.ContentService;
+import com.taotao.pojo.TbContent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 内容管理Controller
+ * <p>Title: ContentController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Controller
+public class ContentController {
+
+
+	@Autowired
+	private ContentService contentService;
+	
+	@RequestMapping("/content/save")
+	@ResponseBody
+	public TaotaoResult addContent(TbContent content) {
+		TaotaoResult result = contentService.addContent(content);
+		return result;
+	}
+
+	@RequestMapping("/content/delete")
+	@ResponseBody
+	public TaotaoResult delContent(Long ids[]) {
+		TaotaoResult result = contentService.delContent(ids);
+		return result;
+	}
+
+
+	@RequestMapping("/content/query/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(Long categoryId, Integer page, Integer rows) {
+		//根据cid查询轮播图内容列表
+		return contentService.getContentListByCid(categoryId, page, rows);
+	}
+}
